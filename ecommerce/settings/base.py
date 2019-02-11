ef14c9f506df79332b421d7687451825cb0327d4
imran1234567 <imran@kgec.edu.in>
2019-02-11 12:28:50 +0530

    add new

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -125,8 +125,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'plutus',
+        'USER':'plutususer',
+        'PASSWORD':'password',
+        'HOST':'localhost',
+        'PORT':'',
     }
 }
 
