4473eac97bfd5140d23b9d5ccf2f321c1e616f3b
imran1234567 <sk2read@gmail.com>
2019-04-12 16:46:54 +0530

    nav bar change

--- products/models.py
+++ products/models.py
@@ -9,6 +9,9 @@ from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 from ecommerce.utils import unique_slug_generator, get_filename
 
+
+
+
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
